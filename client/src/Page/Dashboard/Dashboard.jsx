55d6f710cb6962ef8c2d76bc4040d1952758bccf
Sheila-a <sheila.codes.m@gmail.com>
2023-05-10 17:35:10 +0100

    Dashboard COMPLETE

--- client/src/Page/Dashboard/Dashboard.jsx
+++ client/src/Page/Dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
+import DashMain from "../../Layout/DashMain/DashMain";
 import Sidebar from "../../Layout/Sidebar/Sidebar";
 import design from "./style.module.css";
 const Dashboard = () => {
   return (
     <div className={design.Dashboard_wrapper}>
       <Sidebar />
-      <div style={{ width: "70%" }}></div>
+      <DashMain />
     </div>
   );
 };
