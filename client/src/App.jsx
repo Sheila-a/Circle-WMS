05a3a0e321be8b737601016b23b0675719aaa55a
Sheila-a <sheila.codes.m@gmail.com>
2023-05-17 18:44:10 +0100

    Fix Leaderboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./userAuthenticate/Login/Login";
 import Signup from "./userAuthenticate/Signup/Signup";
 import Home from "./Page/Home/Home";
-import DashMain from "./Layout/DashMain/DashMain"
-import Footer from "./Components/Footer/Footer"
+import DashMain from "./Layout/DashMain/DashMain";
+import Footer from "./Components/Footer/Footer";
 // import Recycle from "./Page/Recycle/Recycle"
-import RecycleMain  from "./Layout/RecycleMain/RecycleMain"
+import RecycleMain from "./Layout/RecycleMain/RecycleMain";
 import Settings from "./Page/Settings/SettingsPage/Settings";
 import EditPersonal from "./Page/Settings/SettingsComponents/EditPersonal/EditPersonal";
 import EditPassword from "./Page/Settings/SettingsComponents/EditPassword/EditPassword";
@@ -14,34 +14,31 @@ import PrivateRoutes from "./userAuthenticate/PrivateRoutes";
 import PublicRoute from "./userAuthenticate/PublicRoute";
 import LeaderMain from "./Layout/LeaderMain/LeaderMain";
 
-
 function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="dashboard" element={<PrivateRoutes />}>
+          <Route path="" element={<DashMain />} />
+          <Route path="start-recycling" element={<RecycleMain />} />
+          <Route path="leaderboard" element={<LeaderMain />} />
 
-    return (
-      <>
-        <Routes>
-          <Route path="dashboard" element={<PrivateRoutes/>}>          
-            <Route path="" element={<DashMain />} />
-            <Route path="start-recycling" element={<RecycleMain />} />
-            <Route path="leaderboard" element={<LeaderMain />} />
-            
-            <Route path="settings" element={<Settings/>}>
-                <Route path="personal" element={<EditPersonal/>}/>
-                <Route path="password" element={<EditPassword/>}/>
-            </Route>
+          <Route path="settings" element={<Settings />}>
+            <Route path="personal" element={<EditPersonal />} />
+            <Route path="password" element={<EditPassword />} />
           </Route>
+        </Route>
 
-          <Route path="/" element={<PublicRoute/>}>
-                <Route path="" element={<Home />} />
-                <Route path="/signup" element={<Signup  />} />
-                <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
+        <Route path="/" element={<PublicRoute />}>
+          <Route path="" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
 
-          <Footer />
-      </>
-    );
+      <Footer />
+    </>
+  );
 }
-
 
 export default App;
