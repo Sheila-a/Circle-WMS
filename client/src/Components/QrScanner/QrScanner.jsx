1198bd795493cfab2ebd60ecd8837449df6bf0b4
Sheila-a <sheila.codes.m@gmail.com>
2023-05-17 15:42:50 +0100

    Add Processing to recycle page

--- client/src/Components/QrScanner/QrScanner.jsx
+++ client/src/Components/QrScanner/QrScanner.jsx
@@ -1,49 +1,91 @@
-import   { useState, useRef } from "react";
-import jsQR from "jsqr"
-import Button from "../Button/Button"
-import design from "./style.module.css"
+import jsQR from "jsqr";
+import PropTypes from "prop-types";
+import Button from "../Button/Button";
+import design from "./style.module.css";
+import { useState, useRef } from "react";
 
-const QRCodeScanner = () => {
+const QRCodeScanner = ({ onRecycledData, onProcessed }) => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const [result, setResult] = useState("");
 
   const startScan = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(stream => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
         videoRef.current.srcObject = stream;
         videoRef.current.play();
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error accessing camera:", error);
       });
 
     const canvas = canvasRef.current.getContext("2d");
     canvas.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
     const intervalId = setInterval(() => {
-      canvas.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
-      const imageData = canvas.getImageData(0, 0, canvasRef.current.width, canvasRef.current.height);
+      canvas.drawImage(
+        videoRef.current,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      const imageData = canvas.getImageData(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
       const code = jsQR(imageData.data, imageData.width, imageData.height);
       if (code) {
         setResult(code.data);
         clearInterval(intervalId);
-        videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+        onRecycledData(code.data);
       }
     }, 100);
   };
 
+  const handleProcessed = () => {
+    onProcessed();
+  };
+
   return (
-    <div  className={design.QR_container}>
-      <Button content="Scan QR Code" onClick={startScan} style={{backgroundColor:"#8BC34A", border:"none", marginLeft:"0"}}/>
-      <p>Make sure the QR code is inside the viewfinder rectangle to start scanning</p>
+    <div className={design.QR_container}>
+      <Button
+        content="Scan QR Code"
+        onClick={startScan}
+        style={{ backgroundColor: "#8BC34A", border: "none", marginLeft: "0" }}
+      />
+      <p>
+        Make sure the QR code is inside the viewfinder rectangle to start
+        scanning
+      </p>
       <video ref={videoRef} style={{ display: "none" }}></video>
       <canvas ref={canvasRef} className={design.QrCanvas}></canvas>
-      <div className={design.QR_result_container} >
-        {result && <p style={{  margin: "0" }}>You have recycled {result}</p>}
+      <div className={design.QR_result_container}>
+        {result && (
+          <Button
+            onClick={handleProcessed}
+            content={`You have recycled ` + result}
+            style={{
+              backgroundColor: "#8BC34A",
+              border: "none",
+              marginLeft: "0",
+              width: "100%",
+              textAlign: "center",
+              fontWeigh: "300",
+            }}
+          />
+        )}
       </div>
-      {/* {result && <p>You have recycled {result}</p>} */}
     </div>
   );
+};
+
+QRCodeScanner.propTypes = {
+  onRecycledData: PropTypes.func.isRequired,
+  onProcessed: PropTypes.func.isRequired,
 };
 
 export default QRCodeScanner;
--- client/src/Page/Settings/SettingsComponents/Header/Sheader.jsx
+++ client/src/Page/Settings/SettingsComponents/Header/Sheader.jsx
@@ -9,29 +9,6 @@ function Sheader() {
   const { auth } = useContext(AuthContext);
 
   return (
-    // <div>
-    //   <header className={styles["sheader"]}>
-    //     <section>
-    //       <h2>Hello {auth?.user?.fullName}</h2>
-    //       <p>Make Changes that are personalized to you.</p>
-
-    //       <Link to="start-recycling">
-    //         <Button
-    //           content="Start recycling"
-    //           style={{
-    //             backgroundColor: "#8BC34A",
-    //             border: "none",
-    //             marginLeft: "0",
-    //           }}
-    //         />
-    //       </Link>
-    //     </section>
-
-    //     <figure className={styles["header-img-container"]}>
-    //       <img src={img} alt="" className={styles["header-img"]} />
-    //     </figure>
-    //   </header>
-    // </div>
     <div className={styles["sheader"]}>
       <div>
         <h2>Hello {auth?.user?.fullName}</h2>
@@ -49,26 +26,6 @@ function Sheader() {
       </div>
       <img src={Hero} />
     </div>
-    // <div className={styles["sheader"]>
-    // <div>
-    //   <h2>Hello {auth?.user?.fullName}</h2>
-    //   <p>
-    //     We are on a mission to make the world cleaner through recycling and
-    //     blockchain technology
-    //   </p>
-    //   <Link to="start-recycling">
-    //     <Button
-    //       content="Start recycling"
-    //       style={{
-    //         backgroundColor: "#8BC34A",
-    //         border: "none",
-    //         marginLeft: "0",
-    //       }}
-    //     />
-    //   </Link>
-    // </div>
-    // <img src={Hero} />
-    // </div>
   );
 }
 
